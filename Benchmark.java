fdac1492f71c9050420f6f3712d5f6b867906160
cyu2019 <cyu2019@pingry.org>
2017-01-23 14:53:38 -0500

    Java Benchmark

---
+++ Benchmark.java
@@ -0,0 +1,36 @@
+package application;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+
+public class Benchmark {
+	public static void main(String[] args){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat m = Highgui.imread("benchmark.jpg");
+		int x = 0;
+		int y = 0;
+		int count = 0;
+		// BGR
+		double[] color = {255.0,255.0,0};
+		for (int i = 0; i < m.rows(); i++){
+			for (int j = 0; j < m.cols(); j++){
+				if (threshold(m.get(i,j),color,30)){
+					x += j;
+					y += i;
+					count++;
+
+				}
+			}
+		}
+		System.out.format("(%d,%d)", x/count, y/count);
+	}
+	public static boolean threshold(double[] a, double[] b, int t){
+		for (int i = 0; i < a.length; i++)
+			if (Math.abs(a[i] - b[i]) > t)
+				return false;
+		return true;
+	}
+}
