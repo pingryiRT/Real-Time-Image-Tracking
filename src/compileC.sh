8b528d592b4581af4cbdccdaad855db3104e5c53
mcgrizzz <mcgrizzz@gmail.com>
2017-03-31 14:37:11 -0400

    Video output working, beginning of thresholding is there

--- src/compileC.sh
+++ src/compileC.sh
@@ -1,1 +1,1 @@
-g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 -L/usr/local/lib/ -g -o binary  main.cpp -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_stitching
+g++ -o main main.cpp `pkg-config opencv --cflags --libs`
