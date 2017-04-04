798b037d693bc77c9888f2a087719caa6d877f5f
mcgrizzz <mcgrizzz@gmail.com>
2017-04-04 12:56:50 -0400

    Thesholding done, maybe add some more GUI stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <stdio.h>
-#include "opencv2/opencv.hpp"
-#include "opencv2/videoio.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+#include <stdio.h>
+using namespace cv;
 int threshold (uchar targetB, uchar targetG, uchar targetR, uchar b, uchar g, uchar r, int t ){
 	if (MAX(targetB,b)-MIN(targetB,b) > t)
 		return false;
@@ -12,25 +13,59 @@ int threshold (uchar targetB, uchar targetG, uchar targetR, uchar b, uchar g, uc
 		return false;
 	return true;
 };
-//using namespace cv;
-int main(int argc, char** argv)
-{
-    cv::VideoCapture cap;
-    // open the default camera, use something different from 0 otherwise;
-    // Check VideoCapture documentation.
-    if(!cap.open(0))
-        return 0;
-    while(true)
-    {
-          cv::Mat frame;
-          cap >> frame;
-          if( frame.empty() ) break; // end of video stream
-          imshow("camera", frame);
-          if( cv::waitKey(10) == 27 )
-		break; // stop capturing by pressing ESC 
-    }
-    // the camera will be closed automatically upon exit
-    // cap.close();
-    return 0;
+int main(int argc, char** argv){
+	VideoCapture cap(0);
+	// open the default camera, use something different from 0 otherwise;
+	// Check VideoCapture documentation.
+	if(!cap.open(0))
+		return 0;
+	Vec3b color;
+	Scalar low;
+	Scalar high;
+	int t = 20;       
+	int tracking = false;
+	while(true){
+		Mat frame;
+		cap >> frame;
+		if(frame.empty())
+			break;
+		if (tracking){
+			Mat showframe;
+			inRange(frame, low, high, showframe);
+			imshow("camera", showframe);
+			int count = 0;
+			int x = 0;
+			int y = 0;
+			for(int i = 0; i < frame.rows; i++){
+				for(int j = 0; j < frame.cols; j++){
+					cv::Vec3b pixel = frame.at<cv::Vec3b>(cv::Point(j,i));
+					if (threshold(color.val[0],color.val[1],color.val[2],pixel.val[0],pixel.val[1],pixel.val[2],t)){
+						x += j;
+						y += i;
+						count += 1;
+					}
+				}
+			}
+			if (count == 0){
+				printf("(0,0)\n");
+			}else{
+				printf("(%d,%d)\n",x/count,y/count);
+			}	
+		}else{
+			imshow("camera", frame);
+		}
+		int key = waitKey(10);
+		if( key == 27 ){
+			break;
+		}
+		else if (key == 32){
+			tracking = !tracking;
+			color = frame.at<Vec3b>(Point(frame.cols/2,frame.rows/2));
+			low = Scalar(color.val[0]-t,color.val[1]-t,color.val[2]-t);
+			high = Scalar(color.val[0]+t,color.val[1]+t,color.val[2]+t);
+		}
+	}
+	return 0;
 }
- 	 	 
+
+
